f2e400dac1a6b6ff806749af034a39ccf3cd43bf
vagrant <vagrant@localhost.localdomain>
2020-10-06 18:12:12 +0000

    layouts

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,7 +5,7 @@ class ShopsController < ApplicationController
 
    	def show
  		 @shop = Shop.find(params[:id])
-     @reviews = @shop.reviews.page(params[:page]).per(5)
+     @reviews = @shop.reviews.page(params[:page]).per(3)
   	end
 
   	def top
