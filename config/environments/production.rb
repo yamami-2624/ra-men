7a4b6c0d35d06c51a12d686813bfb46e245c2500
vagrant <vagrant@localhost.localdomain>
2020-10-10 13:40:36 +0000

    deploy2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.css_compressor = :sass
+  config.assets.enabled = true
+  config.assets.compile = true
 end
