79960a86d767366954ea41d45aff580179670fba
vagrant <vagrant@localhost.localdomain>
2020-10-12 06:25:02 +0000

    device

--- db/schema.rb
+++ db/schema.rb
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_101826) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
+    t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
