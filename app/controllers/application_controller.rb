290d516b5bac4e0e5a053eb633d955c5b2278869
vagrant <vagrant@localhost.localdomain>
2020-10-12 06:50:52 +0000

    device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+protected
 	def after_sign_in_path_for(resource)
     	if user_signed_in?
        	   shops_top_path
@@ -25,9 +26,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+end
 
