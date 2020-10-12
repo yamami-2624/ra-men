fed764afbeafaf9fab17b4e1290f18b6dc16fabf
vagrant <vagrant@localhost.localdomain>
2020-10-12 11:48:45 +0000

    layput

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ protected
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :email])
   end
 end
 
